6ef415bcd88111ae463a76624647e519320765fd
Mateus Caetano <mateus-ramoshd@hotmail.com>
2024-11-22 10:01:24 -0300

    add gamification math

--- src/modules/gamification.ts
+++ src/modules/gamification.ts
@@ -1,5 +1,6 @@
 const BASE_EXPERIENCE = 20
 const EXPERIENCE_FACTOR = 1.3
+
 export function calculateLevelFromExperience(experience: number) {
   return (
     Math.floor(
@@ -8,15 +9,10 @@ export function calculateLevelFromExperience(experience: number) {
     ) + 1
   )
 }
-export function calculateExperienceToLevel(level: number) {
-  if (level === 1) return 0 // Nível 1 começa com 0 XP adicional
-  return Math.floor(BASE_EXPERIENCE * EXPERIENCE_FACTOR ** (level - 1))
-}
 
-export function calculateTotalExperienceForLevel(level: number): number {
-  if (level === 1) return calculateExperienceToLevel(1) // Nível 1 começa com 0 XP adicional
-  return (
-    calculateExperienceToLevel(level) +
-    calculateTotalExperienceForLevel(level - 1)
+export function calculateExperienceForNextLevel(level: number) {
+  return Math.floor(
+    BASE_EXPERIENCE *
+      ((EXPERIENCE_FACTOR ** level - 1) / (EXPERIENCE_FACTOR - 1))
   )
 }
